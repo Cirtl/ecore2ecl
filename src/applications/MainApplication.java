21cbeef69e3756d717de0d41f0e018c6ccd698f3
CIRTL <2376846435@qq.com>
2023-02-10 17:40:29 +0800

    javafx主界面函数

---
+++ src/applications/MainApplication.java
@@ -0,0 +1,16 @@
+package applications;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
